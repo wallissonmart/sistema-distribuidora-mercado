dcfb560b5132f52870c898dc22ac48eb3e9983f9
Wallisson Martins <wallissonmartins37@gmail.com>
2022-08-02 13:02:18 -0300

    teste heroku

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','shopping_db') or die('Falha ao conectar ao banco de dados!');
+$conn = mysqli_connect('us-cdbr-east-06.cleardb.net','b9a0d3a1f05872','f56cb28e','heroku_375e7dceb007d23') or die('Falha ao conectar ao banco de dados!');
 
 ?>
